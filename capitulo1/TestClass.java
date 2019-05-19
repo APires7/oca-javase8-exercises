798eaac9e8a72f5d6e878c8751c8693b0c01e8fd
Allan Victor Pires <allan_ve_pires@hotmail.com>
2019-05-18 23:52:30 -0300

    Exercicio 1

---
+++ capitulo1/TestClass.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package capitulo1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * @author Allan
+ *
+ */
+public class TestClass {
+
+	public static void main(String[] args) throws IOException {
+		// Assegura que o diretório será criado
+		Files.createDirectories(Paths.get("logs"));
+
+		// Obtém a data a ser usada no nome do arquivo
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMMdd_hhmm");
+
+		LocalDateTime now = LocalDateTime.now();
+		String date = now.format(df);
+
+		// Define o nome do arquivo no diretório logs
+		String logFileName = "logs\\testlog-" + date + ".txt";
+
+		// Define Logger
+		FileHandler myFileHandler = new FileHandler(logFileName);
+		myFileHandler.setFormatter(new SimpleFormatter());
+		Logger ocajLogger = Logger.getLogger("OCAJ Logger");
+		ocajLogger.setLevel(Level.ALL);
+		ocajLogger.addHandler(myFileHandler);
+
+		// Mensagem de log
+		ocajLogger.info("\nThis a logged information message. ");
+
+		// Fecha o arquivo
+		myFileHandler.close();
+
+	}
+
+}
