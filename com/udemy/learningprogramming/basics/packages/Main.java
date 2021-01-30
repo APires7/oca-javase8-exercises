927eac8e90f44e21364a2c7e875723858185d7fa
Allan <allanpires.a7@gmail.com>
2021-01-30 08:55:37 -0300

    Implementing classes from Udemy course

---
+++ com/udemy/learningprogramming/basics/packages/Main.java
@@ -0,0 +1,13 @@
+package com.udemy.learningprogramming.basics.packages;
+
+// import java.util.*;
+import java.util.Random;
+
+public class Main {
+	
+	 public static void main(String[] args) {
+	        Random random = new Random();
+	        System.out.println(random.nextInt(5));
+	    }
+
+}
